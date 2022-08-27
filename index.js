c892a1de78375b16292e67a3e0d502f85bfd509c
Jaime González <jimmygzzmtz@gmail.com>
2022-08-27 05:54:30 -0500

    Added Large Screen Layout

--- index.js
+++ index.js
@@ -66,135 +66,150 @@ function App(props) {
                         <ion-title size="large" class="centerText">Jaime González CV</ion-title>
                     </ion-toolbar>
                 </ion-header>
-                <ion-card mode="ios">
-                    <img src="assets/profile.webp" />
-                    <ion-card-header>
-                        <ion-card-subtitle>Software Developer</ion-card-subtitle>
-                        <ion-card-title>Jaime Alberto González Martínez</ion-card-title>
-                    </ion-card-header>
-                    <ion-card-content>
-                        <h2>E-mail</h2>
-                        <a href="mailto:jimmygzzmtz@gmail.com">jimmygzzmtz@gmail.com</a>
-                        <h2>LinkedIn</h2>
-                        <a href="https://www.linkedin.com/in/jaime-gonz%C3%A1lez-005758127/" target="_blank">
-                            Jaime González
-                        </a>
-                        <h2>GitHub</h2>
-                        <a href="https://www.github.com/jimmygzzmtz" target="_blank">https://www.github.com/jimmygzzmtz</a>
-                    </ion-card-content>
-                </ion-card>
-    
-                <ion-card mode="ios">
-                    <ion-card-header>
-                        <ion-card-title>Education</ion-card-title>
-                    </ion-card-header>
-                    <ion-card-content>
-                        <h2>2019 - 2020</h2>
-                        MSc Artificial Intelligence at University of Essex
-                        <br /><br />
-                        <h2>2016 - 2020</h2>
-                        B.S. Computer Science and Technology (BCT) at ITESM-Campus Monterrey
-                        <br />
-                        Cumulative GPA: 96.3
-                    </ion-card-content>
-                </ion-card>
-    
-                <ion-card mode="ios">
-                    <ion-card-header>
-                        <ion-card-title>Skills</ion-card-title>
-                    </ion-card-header>
-                    <ion-card-content>
-                        <h2>Programming Languages</h2>
-                        <ul>
-                            <li>7 years: Java</li>
-                            <li>5 years: C++</li>
-                            <li>3 years: Python, TypeScript, JavaScript</li>
-                        </ul>
-                        <h2>Awards</h2>
-                        <ul>
-                            <li>Academic Excellence at ITESM (2016-2020)</li>
-                            <li>1st Place in the HackPrepas-ITESM (2015)</li>
-                        </ul>
-                        <h2>Technologies</h2>
-                        <ul>
-                            <li>HTML, CSS, JQuery, Express, SQL, MongoDB (NoSQL), Git, Google Cloud, Heroku, Android SDK, iOS SDK, Ionic, Angular, React, Puppeteer, Unix/Bash, TensorFlow, Preact</li>
-                        </ul>
-                    </ion-card-content>
-                </ion-card>
-    
-                <ion-card mode="ios">
-                    <ion-card-header>
-                        <ion-card-title>Work Experience</ion-card-title>
-                    </ion-card-header>
-                    <ion-card-content>
-                        <h2>February 2021 - November 2021</h2>
-                        Full Stack Engineer at Intelimotor
-                        <br /><br />
-                        <h2>November 2021 - May 2022</h2>
-                        Senior Developer at Onis Solutions
-                        <br /><br />
-                        <h2>June 2022 - Present</h2>
-                        Co-founder at Nuxio Software
-                    </ion-card-content>
-                </ion-card>
-    
-                <ion-card mode="ios">
-                    <div style="width: 100%; background-color: white;">
-                        <img src="assets/nuxio-logo.webp" class="centerImage" style="padding-top: 10px; padding-bottom: 10px;" />
-                    </div>
-                    <ion-card-header>
-                        <ion-card-subtitle>Current Role</ion-card-subtitle>
-                        <ion-card-title>Co-founder at Nuxio Software</ion-card-title>
-                    </ion-card-header>
-                    <ion-card-content>
-                        <h2>Nuxio Software</h2>
-                        Your allies in software development & modernization.
-                        <br /><br />
-                        <h2>Our services</h2>
-                        Develop an application from the ground up or modernize your current application with the latest web industry standards.
-                        <br /><br />
-                        <h2>More Information</h2>
-                        <a href="https://nuxio.mx/" target="_blank">Visit our website</a>
-                    </ion-card-content>
-                </ion-card>
+                <ion-grid>
+                    <ion-row class="centerButton">
+                        <ion-col size="12" size-sm>
+                            <ion-card mode="ios">
+                                <img src="assets/profile.webp" />
+                                <ion-card-header>
+                                    <ion-card-subtitle>Software Developer</ion-card-subtitle>
+                                    <ion-card-title>Jaime Alberto González Martínez</ion-card-title>
+                                </ion-card-header>
+                                <ion-card-content>
+                                    <h2>E-mail</h2>
+                                    <a href="mailto:jimmygzzmtz@gmail.com">jimmygzzmtz@gmail.com</a>
+                                    <h2>LinkedIn</h2>
+                                    <a href="https://www.linkedin.com/in/jaime-gonz%C3%A1lez-005758127/" target="_blank">
+                                        Jaime González
+                                    </a>
+                                    <h2>GitHub</h2>
+                                    <a href="https://www.github.com/jimmygzzmtz"
+                                        target="_blank">https://www.github.com/jimmygzzmtz</a>
+                                </ion-card-content>
+                            </ion-card>
+                        </ion-col>
+                        <ion-col size="12" size-sm>
+                            <ion-card mode="ios">
+                                <ion-card-header>
+                                    <ion-card-title>Education</ion-card-title>
+                                </ion-card-header>
+                                <ion-card-content>
+                                    <h2>2019 - 2020</h2>
+                                    MSc Artificial Intelligence at University of Essex
+                                    <br /><br />
+                                    <h2>2016 - 2020</h2>
+                                    B.S. Computer Science and Technology (BCT) at ITESM-Campus Monterrey
+                                    <br />
+                                    Cumulative GPA: 96.3
+                                </ion-card-content>
+                            </ion-card>
+                        </ion-col>
+                        <ion-col size="12" size-sm>
+                            <ion-card mode="ios">
+                                <ion-card-header>
+                                    <ion-card-title>Skills</ion-card-title>
+                                </ion-card-header>
+                                <ion-card-content>
+                                    <h2>Programming Languages</h2>
+                                    <ul>
+                                        <li>7 years: Java</li>
+                                        <li>5 years: C++</li>
+                                        <li>3 years: Python, TypeScript, JavaScript</li>
+                                    </ul>
+                                    <h2>Awards</h2>
+                                    <ul>
+                                        <li>Academic Excellence at ITESM (2016-2020)</li>
+                                        <li>1st Place in the HackPrepas-ITESM (2015)</li>
+                                    </ul>
+                                    <h2>Technologies</h2>
+                                    <ul>
+                                        <li>HTML, CSS, JQuery, Express, SQL, MongoDB (NoSQL), Git, Google Cloud, Heroku, Android SDK, iOS SDK, Ionic, Angular, React, Puppeteer, Unix/Bash, TensorFlow, Preact</li>
+                                    </ul>
+                                </ion-card-content>
+                            </ion-card>
+                        </ion-col>
+                        <ion-col size="12" size-sm>
+                            <ion-card mode="ios">
+                                <ion-card-header>
+                                    <ion-card-title>Work Experience</ion-card-title>
+                                </ion-card-header>
+                                <ion-card-content>
+                                    <h2>February 2021 - November 2021</h2>
+                                    Full Stack Engineer at Intelimotor
+                                    <br /><br />
+                                    <h2>November 2021 - May 2022</h2>
+                                    Senior Developer at Onis Solutions
+                                    <br /><br />
+                                    <h2>June 2022 - Present</h2>
+                                    Co-founder at Nuxio Software
+                                </ion-card-content>
+                            </ion-card>
+                        </ion-col>
+                        <ion-col size="12" size-sm>
+                            <ion-card mode="ios">
+                                <div style="width: 100%; background-color: white;">
+                                    <img src="assets/nuxio-logo.webp" class="centerImage"
+                                        style="padding-top: 10px; padding-bottom: 10px;" />
+                                </div>
+                                <ion-card-header>
+                                    <ion-card-subtitle>Current Role</ion-card-subtitle>
+                                    <ion-card-title>Co-founder at Nuxio Software</ion-card-title>
+                                </ion-card-header>
+                                <ion-card-content>
+                                    <h2>Nuxio Software</h2>
+                                    Your allies in software development & modernization.
+                                    <br /><br />
+                                    <h2>Our services</h2>
+                                    Develop an application from the ground up or modernize your current application with the latest web industry standards.
+                                    <br /><br />
+                                    <h2>More Information</h2>
+                                    <a href="https://nuxio.mx/" target="_blank">Visit our website</a>
+                                </ion-card-content>
+                            </ion-card>
+                        </ion-col>
+                    </ion-row>
+                    <ion-row class="centerButton">
+                        ${showingProjects && projects.map((project) => html`
+                        <ion-col size="12" size-sm>
+                            <ion-card mode="ios">
+                                <img src=${project.screenshot} style="width: 100%" />
+                                <img src=${project.icon} height="50" width="50" class="projectIcon" />
+                                <ion-card-header>
+                                    <ion-card-subtitle>Project</ion-card-subtitle>
+                                    <ion-card-title>${project.name}</ion-card-title>
+                                </ion-card-header>
+                                <ion-card-content>
+                                    <h2>Timeframe</h2>
+                                    ${project.timeframe}
+                                    <br /><br />
+                                    <h2>Description</h2>
+                                    ${project.description}
+                                    <br /><br />
+                                    <h2>Details</h2>
+                                    <ul>
+                                        ${project.details.map((detail) => html`
+                                        <li>${detail}</li>
+                                        `)}
+                                    </ul>
+                                    ${project.links && html`
+                                    <h2>Links</h2>
+                                    ${project.links.map((link) => html`
+                                    <a href=${link.url} target="_blank">${link.name}</a>
+                                    <br />
+                                    `)}
+                                    `}
+                                </ion-card-content>
+                            </ion-card>
+                        </ion-col>
+                        `
+    )}
+                    </ion-row>
+                </ion-grid>
     
                 <div class="centerButton">
-                    <ion-button mode="ios" class="center" onClick=${() =>
-            setShowingProjects(!showingProjects)}>${showingProjects ? 'Hide' : 'Show'} Projects</ion-button>
+                    <ion-button mode="ios" class="center" onClick=${()=>
+                        setShowingProjects(!showingProjects)}>${showingProjects ? 'Hide' : 'Show'} Projects</ion-button>
                 </div>
-    
-                ${showingProjects && projects.map((project) => html`
-                <ion-card mode="ios">
-                    <img src=${project.screenshot} style="width: 100%" />
-                    <img src=${project.icon} height="50" width="50" class="projectIcon" />
-                    <ion-card-header>
-                        <ion-card-subtitle>Project</ion-card-subtitle>
-                        <ion-card-title>${project.name}</ion-card-title>
-                    </ion-card-header>
-                    <ion-card-content>
-                        <h2>Timeframe</h2>
-                        ${project.timeframe}
-                        <br /><br />
-                        <h2>Description</h2>
-                        ${project.description}
-                        <br /><br />
-                        <h2>Details</h2>
-                        <ul>
-                            ${project.details.map((detail) => html`
-                            <li>${detail}</li>
-                            `)}
-                        </ul>
-                        ${project.links && html`
-                        <h2>Links</h2>
-                        ${project.links.map((link) => html`
-                        <a href=${link.url} target="_blank">${link.name}</a>
-                        <br />
-                        `)}
-                        `}
-                    </ion-card-content>
-                </ion-card>
-                `
-                        )}
             </ion-content>
         </ion-app>
     </div>
